2727852c95208e78bd4a79c4a451c850b9ebf2e9
rayhanMahmud <1605035@ugrad.cse.buet.ac.bd>
2020-07-19 19:10:35 +0600

    first commit

---
+++ js/photoGallery.js
@@ -0,0 +1,44 @@
+function upDate(previewPic){
+    
+    var language=previewPic.lang;
+    console.log(language);
+
+    var bengali=document.getElementsByClassName("bengali");
+    var english=document.getElementsByClassName("english");
+
+    if(language=="bn"){
+        for (element of bengali) {
+            element.style.display="inline-block";
+        }
+        for (element of english) {
+            element.style.display="none";
+        }
+    }else if(language=="en"){
+        for (element of english) {
+            element.style.display="inline-block";
+        }
+        for (element of bengali) {
+            element.style.display="none";
+        }
+
+    }
+}
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
